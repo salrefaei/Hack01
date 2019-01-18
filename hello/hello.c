cb7ee442c4042b097f2550716e055b355536d8e8
salrefeai <42729693+salrefeai@users.noreply.github.com>
2019-01-17 18:42:01 -0600

    Add files via upload

---
+++ hello/hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Sultan 
+ * Date: 2019/15/01
+ *
+ * Sultan Alrefaei printing
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Sultan Alrefaei");
+
+  return 0;
+}
